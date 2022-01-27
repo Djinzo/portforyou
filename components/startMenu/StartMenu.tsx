7f253b970b095556df6f40fca53cfc604114468f
Ar-mane <abderrahmane.arache@gmail.com>
2022-01-27 22:59:36 +0100

    fix(tsx) : remove unused props

--- components/startMenu/StartMenu.tsx
+++ components/startMenu/StartMenu.tsx
@@ -12,7 +12,7 @@ import StartMenu from "./StartMenu.stye";
 const StartMenus = () => {
   return (
     <StartMenu.Container>
-      <StartMenu.Options valid>
+      <StartMenu.Options>
         <StartMenu.ImageIcon src={computerImage} />
         Programes
       </StartMenu.Options>
