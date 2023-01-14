e545555067c69b80f74076cd1300e225176b8830
goulzima Med amine <37093694+Djinzo@users.noreply.github.com>
2023-01-14 19:46:08 +0100

    adding task bar (#9)
    Co-authored-by: mohamed.amine-goulzima <mohamed-amine.goulzima@outlook.com>

---
+++ components/taskBar/taskBar.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import TaskBarStyle from './taskBar.style'
+import WindowsImage from '@assets/windows-0.png'
+import StartMenu from '@components/startMenu/StartMenu';
+
+interface TaskBarProps { }
+
+const TaskBar: React.FC<TaskBarProps> = ({ }): React.ReactElement => {
+   const [showStartMenue, setShowStartMenue] = useState(false);
+
+
+
+   function handelShowStartMenueEvent() {
+      setShowStartMenue(!showStartMenue);
+   }
+
+   return (
+      <TaskBarStyle.Container>
+         {showStartMenue? <StartMenu/>:""}
+         <TaskBarStyle.TaskBar onClick={() => handelShowStartMenueEvent()}>
+            <TaskBarStyle.StartButton>
+               <TaskBarStyle.StartImage src={WindowsImage} width={18} height={18} />
+               <TaskBarStyle.StartText>
+                  Start
+               </TaskBarStyle.StartText>
+            </TaskBarStyle.StartButton>
+            <TaskBarStyle.Time>
+               10:20 PM
+            </TaskBarStyle.Time>
+         </TaskBarStyle.TaskBar>
+      </TaskBarStyle.Container>
+   )
+}
+
+export default TaskBar
--- pages/MainScreen/MainScreen.tsx
+++ pages/MainScreen/MainScreen.tsx
@@ -1,11 +1,12 @@
 import StartMenus from '@components/startMenu/StartMenu'
+import TaskBar from '@components/taskBar/taskBar'
 import { NextPage } from 'next'
 import React from 'react'
 import MainScreenStyle from './MainScreen.style'
 
 const MainScreen: NextPage = () => (
    <MainScreenStyle.Container>
-      <StartMenus />
+      <TaskBar />
    </MainScreenStyle.Container>
 )
 
