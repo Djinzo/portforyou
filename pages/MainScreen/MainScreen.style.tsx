633bf8639cb01ac5e9ebb36bd6d63c1bb4d48ea6
Ar-mane <abderrahmane.arache@gmail.com>
2023-01-15 23:28:25 +0100

    format the intire project

--- pages/MainScreen/MainScreen.style.tsx
+++ pages/MainScreen/MainScreen.style.tsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components'
 const MainScreenStyleContainer = styled.div`
-      margin: 0;
-      padding: 0;
-   `
-
+   margin: 0;
+   padding: 0;
+`
 
 export default MainScreenStyleContainer
